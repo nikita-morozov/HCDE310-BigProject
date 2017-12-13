7c94f97ced7595e117b67cc4edc8881e24743b6d
nikita-morozov <nikita1020@gmail.com>
2017-12-13 08:14:12 -0800

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-mapquestKey = "GbNRuru2gV89bdcHm5TRH1ypIq6nVpCj"
-bingKey = "Aktyff2Yyb8PnS6W0Hn4q7V3KiPKjp1aviaHApvOfF2Qplby-imn-FwTt2wqioP2"
-wsdotKey = "4b53e440-83ed-4f1c-ab05-7d5da71d4e56"
-googleMapKey = "AIzaSyAkEKREhMHlD8RcsK0prnTn_fQJV2MRI7Y"
-weatherKey = "58f923946ae48b1d"
